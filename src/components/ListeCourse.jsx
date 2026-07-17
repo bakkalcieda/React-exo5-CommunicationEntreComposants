0be8fb5bbff0db83a4af70db41e20ce5aa47436f
Bakkalcieda <bakkalci.eda98@gmail.com>
2026-07-17 11:47:30 +0200

    feat: validation ajout produit (vide + doublon) et compteur total

--- src/components/ListeCourse.jsx
+++ src/components/ListeCourse.jsx
@@ -5,22 +5,21 @@ function ListeCourse() {
     const [produits, setProduits] = useState(['Pain', 'Lait', 'Œufs']);
     const [texteInput, setTexteInput] = useState('');
     
-    function ajouterProduit() {
-        const valeur = texteInput.trim();
-        
-        if (valeur === '') {
-            alert('Le champ est vide !');
-            return;
-        }
-        
-        if (produits.includes(valeur)) {
-            alert('Ce produit est déjà dans la liste !');
-            return;
-        }
-        
-        setProduits([...produits, valeur]);
-        setTexteInput('');
+function ajouterProduit() {
+    const valeur = texteInput.trim();
+
+    if (valeur === "") {
+        return;
     }
+
+    if (produits.includes(valeur)) {
+        alert('Ce produit est déjà dans la liste !');
+        return;
+    }
+
+    setProduits([...produits, valeur]);
+    setTexteInput('');
+}
 
     function supprimerProduit(index) {
     const nouvelleListe = produits.filter((_, i) => i !== index);
@@ -34,9 +33,17 @@ function ListeCourse() {
         type="text"
         value={texteInput}
         onChange={(e) => setTexteInput(e.target.value)}
+        // rajout avec  touche enter //
+        onKeyDown={(e) => {
+    if (e.key === 'Enter') {
+    ajouterProduit();
+    }
+}}
         placeholder="Nom du produit"
     />
     <button onClick={ajouterProduit}>Ajouter</button>
+    {/* //rajout compteur// */}
+    <p>Nombre de produits : {produits.length}</p>
 <ul>
         {produits.map((produit, index) => (
         <ProduitCarte
