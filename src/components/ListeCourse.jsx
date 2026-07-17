6d6d1b9020d860e215e1d87255d7e39a39891651
Bakkalcieda <bakkalci.eda98@gmail.com>
2026-07-17 10:54:39 +0200

    Ajout formulaire pour ajouter un produit a la liste

--- src/components/ListeCourse.jsx
+++ src/components/ListeCourse.jsx
@@ -1,11 +1,37 @@
+import { useState } from 'react';
 import ProduitCarte from './ProduitCarte';
 
 function ListeCourse() {
-    const produits = ['Compote', 'Lait', 'Pampers'];
+    const [produits, setProduits] = useState(['Pain', 'Lait', 'Œufs']);
+    const [texteInput, setTexteInput] = useState('');
+    
+    function ajouterProduit() {
+        const valeur = texteInput.trim();
+        
+        if (valeur === '') {
+            alert('Le champ est vide !');
+            return;
+        }
+        
+        if (produits.includes(valeur)) {
+            alert('Ce produit est déjà dans la liste !');
+            return;
+        }
+        
+        setProduits([...produits, valeur]);
+        setTexteInput('');
+    }
     
     return (
         <div>
         <h2>Liste de courses</h2>
+            <input
+        type="text"
+        value={texteInput}
+        onChange={(e) => setTexteInput(e.target.value)}
+        placeholder="Nom du produit"
+    />
+    <button onClick={ajouterProduit}>Ajouter</button>
         <ul>
         {produits.map((produit, index) => (
             <ProduitCarte key={index} nom={produit} />
